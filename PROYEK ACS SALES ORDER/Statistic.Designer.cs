134c864dd098c2c88449df5976a7ab533c1cd4e4
JAW12 <jem.angkasa91@gmail.com>
2019-11-25 09:09:46 +0700

    V35 - statistic income product sudah

--- PROYEK ACS SALES ORDER/Statistic.Designer.cs
+++ PROYEK ACS SALES ORDER/Statistic.Designer.cs
@@ -28,10 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.lblJudul = new System.Windows.Forms.Label();
             this.pbIcon = new System.Windows.Forms.PictureBox();
             this.pbLogo = new System.Windows.Forms.PictureBox();
@@ -39,6 +39,11 @@
             this.lbl_ = new System.Windows.Forms.Label();
             this.lblX = new System.Windows.Forms.Label();
             this.chStatistic = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.cbJenis = new System.Windows.Forms.ComboBox();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.lblUntil = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbIcon)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvTable)).BeginInit();
@@ -82,19 +87,20 @@
             this.dgvTable.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvTable.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dgvTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvTable.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvTable.DefaultCellStyle = dataGridViewCellStyle3;
             this.dgvTable.Location = new System.Drawing.Point(13, 60);
             this.dgvTable.Name = "dgvTable";
             this.dgvTable.ReadOnly = true;
             this.dgvTable.Size = new System.Drawing.Size(875, 217);
             this.dgvTable.TabIndex = 112;
+            this.dgvTable.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTable_CellClick);
             // 
             // lbl_
             // 
@@ -122,25 +128,72 @@
             // 
             // chStatistic
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chStatistic.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chStatistic.Legends.Add(legend1);
+            chartArea3.Name = "ChartArea1";
+            this.chStatistic.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chStatistic.Legends.Add(legend3);
             this.chStatistic.Location = new System.Drawing.Point(13, 284);
             this.chStatistic.Name = "chStatistic";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chStatistic.Series.Add(series1);
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chStatistic.Series.Add(series3);
             this.chStatistic.Size = new System.Drawing.Size(875, 454);
             this.chStatistic.TabIndex = 115;
             this.chStatistic.Text = "chart1";
+            // 
+            // cbJenis
+            // 
+            this.cbJenis.FormattingEnabled = true;
+            this.cbJenis.Location = new System.Drawing.Point(304, 32);
+            this.cbJenis.Name = "cbJenis";
+            this.cbJenis.Size = new System.Drawing.Size(137, 21);
+            this.cbJenis.TabIndex = 116;
+            this.cbJenis.SelectedIndexChanged += new System.EventHandler(this.cbJenis_SelectedIndexChanged);
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.Location = new System.Drawing.Point(447, 33);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(133, 20);
+            this.dtpFrom.TabIndex = 117;
+            // 
+            // dtpTo
+            // 
+            this.dtpTo.Location = new System.Drawing.Point(602, 33);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.Size = new System.Drawing.Size(133, 20);
+            this.dtpTo.TabIndex = 118;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(741, 31);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 119;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // lblUntil
+            // 
+            this.lblUntil.AutoSize = true;
+            this.lblUntil.Location = new System.Drawing.Point(586, 35);
+            this.lblUntil.Name = "lblUntil";
+            this.lblUntil.Size = new System.Drawing.Size(10, 13);
+            this.lblUntil.TabIndex = 120;
+            this.lblUntil.Text = "-";
             // 
             // Statistic
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(900, 750);
+            this.Controls.Add(this.lblUntil);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.dtpTo);
+            this.Controls.Add(this.dtpFrom);
+            this.Controls.Add(this.cbJenis);
             this.Controls.Add(this.chStatistic);
             this.Controls.Add(this.lbl_);
             this.Controls.Add(this.lblX);
@@ -170,5 +223,10 @@
         private System.Windows.Forms.Label lbl_;
         private System.Windows.Forms.Label lblX;
         private System.Windows.Forms.DataVisualization.Charting.Chart chStatistic;
+        private System.Windows.Forms.ComboBox cbJenis;
+        private System.Windows.Forms.DateTimePicker dtpFrom;
+        private System.Windows.Forms.DateTimePicker dtpTo;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Label lblUntil;
     }
 }
--- PROYEK ACS SALES ORDER/Statistic.cs
+++ PROYEK ACS SALES ORDER/Statistic.cs
@@ -33,8 +33,16 @@ namespace PROYEK_ACS_SALES_ORDER_V1
         DataTable dt;
         private void Statistic_VisibleChanged(object sender, EventArgs e)
         {
-            if(tipe == "TP")
+            cbJenis.Items.Clear();
+            idProduk = "";
+            namaProduk = "";
+            if (tipe == "TP")
             {
+                cbJenis.Visible = false;
+                dtpFrom.Visible = false;
+                lblUntil.Visible = false;
+                dtpTo.Visible = false;
+                btnShow.Visible = false;
                 dt = new DataTable();
                 dt = login.db.executeDataTable("SELECT CASE THIRD_PARTY_TYPE WHEN '1' THEN 'Prospect' WHEN '2' THEN 'Customer' WHEN '3' THEN 'Vendor' WHEN '4' THEN 'Others' END AS TYPE, COUNT(THIRD_PARTY_TYPE) AS AMOUNT FROM THIRD_PARTY GROUP BY THIRD_PARTY_TYPE");
                 dgvTable.DataSource = dt;
@@ -67,11 +75,94 @@ namespace PROYEK_ACS_SALES_ORDER_V1
             }
             else if(tipe == "SO")
             {
-                dt = new DataTable();
-                dt = login.db.executeDataTable("SELECT ORDER_STATUS AS STATUS, COUNT(ORDER_STATUS) AS AMOUNT FROM H_SORDER GROUP BY ORDER_STATUS");
-                dgvTable.DataSource = dt;
-                dgvTable.Columns[0].Width = dgvTable.Columns[1].Width + 200;
+                cbJenis.Visible = true;
+              
+                cbJenis.Items.Add("Order Status");
+                cbJenis.Items.Add("Product");
+                cbJenis.SelectedIndex = 0;
 
+            }
+        }
+
+        private void cbJenis_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(tipe == "SO" && cbJenis.Items.Count > 0)
+            {
+                if (cbJenis.SelectedIndex == 0)
+                {
+                    dtpFrom.Visible = false;
+                    lblUntil.Visible = false;
+                    dtpTo.Visible = false;
+                    btnShow.Visible = false;
+                    dt = new DataTable();
+                    dt = login.db.executeDataTable("SELECT ORDER_STATUS AS STATUS, COUNT(ORDER_STATUS) AS AMOUNT FROM H_SORDER GROUP BY ORDER_STATUS");
+                    dgvTable.DataSource = dt;
+                    dgvTable.Columns[0].Width = dgvTable.Columns[1].Width + 200;
+
+                    chStatistic.Series.Clear();
+                    chStatistic.Legends.Clear();
+
+                    //Add a new Legend(if needed) and do some formating
+                    chStatistic.Legends.Add("Status");
+                    chStatistic.Legends[0].LegendStyle = LegendStyle.Table;
+                    chStatistic.Legends[0].Docking = Docking.Bottom;
+                    chStatistic.Legends[0].Alignment = StringAlignment.Center;
+                    chStatistic.Legends[0].Title = "Sales Order Status";
+                    chStatistic.Legends[0].BorderColor = Color.Black;
+
+                    //Add a new chart-series
+                    string seriesname = "SOStatus";
+                    chStatistic.Series.Add(seriesname);
+                    //set the chart-type to "Pie"
+                    chStatistic.Series[seriesname].ChartType = SeriesChartType.Pie;
+                    chStatistic.Series[seriesname].LabelForeColor = Color.White;
+                    chStatistic.Series[seriesname].Font = new Font("default", 10, FontStyle.Bold);
+
+                    //Add some datapoints so the series. in this case you can pass the values to this method
+                    foreach (DataGridViewRow row in dgvTable.Rows)
+                    {
+                        chStatistic.Series[seriesname].Points.AddXY(row.Cells[0].Value, row.Cells[1].Value);
+                    }
+                }
+                else if (cbJenis.SelectedIndex == 1)
+                {
+                    dtpFrom.Visible = true;
+                    lblUntil.Visible = true;
+                    dtpTo.Visible = true;
+                    btnShow.Visible = true;
+                    dt = new DataTable();
+                    String query = $"select * from product";
+
+                    dt = login.db.executeDataTable(query);
+                    dgvTable.DataSource = dt;
+                    dgvTable.Columns[3].Width = 300;
+                    dgvTable.Columns[2].Width = 200;
+                    dgvTable.Columns[4].Visible = false;
+                }
+            }
+        }
+
+        string idProduk = "";
+        string namaProduk = "";
+        private void dgvTable_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            idProduk = "";
+            namaProduk = "";
+            if (tipe == "SO" && cbJenis.SelectedIndex == 1)
+            {
+                int row = e.RowIndex;
+                int col = e.ColumnIndex;
+                if (row > -1 && row < dgvTable.RowCount && col > -1 && col < dgvTable.ColumnCount)
+                {
+                    idProduk = dgvTable.Rows[row].Cells[0].Value.ToString();
+                    namaProduk = dgvTable.Rows[row].Cells[2].Value.ToString();
+                }
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            if(idProduk != "") { 
                 chStatistic.Series.Clear();
                 chStatistic.Legends.Clear();
 
@@ -80,21 +171,23 @@ namespace PROYEK_ACS_SALES_ORDER_V1
                 chStatistic.Legends[0].LegendStyle = LegendStyle.Table;
                 chStatistic.Legends[0].Docking = Docking.Bottom;
                 chStatistic.Legends[0].Alignment = StringAlignment.Center;
-                chStatistic.Legends[0].Title = "Sales Order Status";
+                chStatistic.Legends[0].Title = namaProduk + "'s Cashflow";
                 chStatistic.Legends[0].BorderColor = Color.Black;
 
                 //Add a new chart-series
-                string seriesname = "SOStatus";
+                string seriesname = namaProduk;
                 chStatistic.Series.Add(seriesname);
                 //set the chart-type to "Pie"
-                chStatistic.Series[seriesname].ChartType = SeriesChartType.Pie;
+                chStatistic.Series[seriesname].ChartType = SeriesChartType.Line;
                 chStatistic.Series[seriesname].LabelForeColor = Color.White;
                 chStatistic.Series[seriesname].Font = new Font("default", 10, FontStyle.Bold);
 
+                DataTable dt = new DataTable();
+                dt = login.db.executeDataTable($"SELECT H.ORDER_CREATED_DATE AS TGL, ((D.INPUT_PRICE - D.COSTPRICE) * D.QTY) AS PROFIT FROM D_SORDER D, H_SORDER H WHERE D.ID_PRODUCT = '{idProduk}' AND D.ORDER_ROW_ID = H.ORDER_ROW_ID AND H.ORDER_CREATED_DATE >= TO_DATE('{dtpFrom.Value.ToShortDateString()}', 'DD/MM/YYYY') AND H.ORDER_CREATED_DATE <= TO_DATE('{dtpTo.Value.ToShortDateString()}', 'DD/MM/YYYY')");
                 //Add some datapoints so the series. in this case you can pass the values to this method
-                foreach (DataGridViewRow row in dgvTable.Rows)
+                foreach (DataRow row in dt.Rows)
                 {
-                    chStatistic.Series[seriesname].Points.AddXY(row.Cells[0].Value, row.Cells[1].Value);
+                    chStatistic.Series[seriesname].Points.AddXY(row[0], row[1]);
                 }
             }
         }
